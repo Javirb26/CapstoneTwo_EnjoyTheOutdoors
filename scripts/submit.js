6361b011a3178208a78baf42e831a02755612f3c
Javier Ballester <Javirb26@gmail.com>
2023-11-16 22:58:54 -0500

    fixed prior issue related to submitting

--- scripts/submit.js
+++ scripts/submit.js
@@ -1,23 +1,23 @@
 window.onload = function () {
-    const formEl = document.getElementById("form-submit");
-    const emailInputEl = document.getElementById("email");
-    const toastTrigger = document.getElementById("liveToastBtn");
-    const toastLiveExample = document.getElementById("liveToast");
+        const formEl = document.getElementById("form-submit");
+        const emailInputEl = document.getElementById("email");
+        const toastTrigger = document.getElementById("liveToastBtn");
+        const toastLiveExample = document.getElementById("liveToast");
 
-    // Initialize the toast
+        // Initialize the toast
 
-    // Prevent form submission and reset email input
-    formEl.addEventListener("submit", function (event) {
-      event.preventDefault();
-      // Your form submission logic here (e.g., sending data to a server)
-      if (emailInputEl.value) {
-        if (toastTrigger) {
-          const toastBootstrap = new bootstrap.Toast(toastLiveExample);
+        // Prevent form submission and reset email input
+        formEl.addEventListener("submit", function (event) {
+          event.preventDefault();
+          // Your form submission logic here (e.g., sending data to a server)
+          if (emailInputEl.value) {
+            if (toastTrigger) {
+              const toastBootstrap = new bootstrap.Toast(toastLiveExample);
 
-          toastBootstrap.show();
+              toastBootstrap.show();
 
-          emailInputEl.value = ""; // Reset the email input field
-        }
-      }
-    });
-  };
+              emailInputEl.value = ""; // Reset the email input field
+            }
+          }
+        });
+      };
