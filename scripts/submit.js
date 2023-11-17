6c3693781b922776aaf4c1707edd4c601b99fed9
Javier Ballester <Javirb26@gmail.com>
2023-11-16 21:57:11 -0500

    created submit.js for the call to action section at the bottom of each page

---
+++ scripts/submit.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    const formEl = document.getElementById("form-submit");
+    const emailInputEl = document.getElementById("email");
+    const toastTrigger = document.getElementById("liveToastBtn");
+    const toastLiveExample = document.getElementById("liveToast");
+
+    // Initialize the toast
+
+    // Prevent form submission and reset email input
+    formEl.addEventListener("submit", function (event) {
+      event.preventDefault();
+      // Your form submission logic here (e.g., sending data to a server)
+      if (emailInputEl.value) {
+        if (toastTrigger) {
+          const toastBootstrap = new bootstrap.Toast(toastLiveExample);
+
+          toastBootstrap.show();
+
+          emailInputEl.value = ""; // Reset the email input field
+        }
+      }
+    });
+  };
